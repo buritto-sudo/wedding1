2deb18aa76d69209bf2ccee45e2b1dd9d9427782
buritto-sudo <hakgkr0215@gmail.com>
2026-09-07 22:47:50 +0900

    Update index.tsx

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -56,7 +56,7 @@ export const Location = () => {
             <div className="transportation-icon-wrapper" style={{ margin: 0 }}>
               <CarIcon className="transportation-icon" />
             </div>
-            <div className="heading" style={{ margin: 0, fontWeight: "bold" }}>자가용</div>
+            <div className="heading" style={{ margin: 0, fontWeight: "bold" }}>자가용 및 주차 안내</div>
           </div>
           
           <div className="content" style={{ textAlign: "left", lineHeight: "1.6", width: "100%", wordBreak: "keep-all" }}>
@@ -64,9 +64,29 @@ export const Location = () => {
             <br />
             <b>르엘컨벤션웨딩홀</b> 검색
             <br />
-            - 주차권 뷔페에서 발급-백화점,호텔,별관주차장
             <br />
-            제1대체주차장-극동(이어도)주차장<도보 3분 뷔페에서 주차권발급> 울산남구 달삼로82
+            <b>[본관 주차장]</b>
+            <br />
+            · 롯데백화점, 롯데호텔, 별관 주차장 (뷔페에서 주차권 발급)
+            <br />
+            <br />
+            <b>[대체 주차장 안내]</b>
+            <br />
+            · <b>제1대체</b> : 극동(이어도)주차장 (도보 3분 / 뷔페에서 주차권 발급)
+            <br />
+            &nbsp;&nbsp;울산 남구 달삼로 82
+            <br />
+            · <b>제2대체</b> : 공구월드 상가 옆 공영주차장 (도보 7분 / 주말 무료)
+            <br />
+            &nbsp;&nbsp;울산 남구 갈밭로 16
+            <br />
+            · <b>제3대체</b> : 삼산동 1공영주차장 (도보 7분 / 주말 무료)
+            <br />
+            &nbsp;&nbsp;울산 남구 삼산동 1622-3
+            <br />
+            · <b>제4대체</b> : 농수산물 하이파킹 유료주차장 (도보 7분 / 뷔페에서 주차권 발급)
+            <br />
+            &nbsp;&nbsp;울산 남구 삼산동 1622-1
           </div>
         </div>
       </LazyDiv>
