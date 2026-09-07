e60f6c4602f9373980fb9931c60e887564256214
buritto-sudo <hakgkr0215@gmail.com>
2026-09-07 15:26:25 +0900

    Update index.tsx

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -24,16 +24,16 @@ export const Invitation = () => {
         <div className="break" />
 
         {/* 초대 문구 */}
-        <div className="content">싱그러운 여름 향기 가득한 날</div>
-        <div className="content">소중한 분들을 모시고</div>
-        <div className="content">사랑의 약속을 하려고 합니다.</div>
+        <div className="content">우주를 한 사람으로 축소하고</div>
+        <div className="content">그 한 사람을 신으로 확대하면</div>
+        <div className="content">그것이 바로 사랑이기에</div>
         <div className="break" />
-        <div className="content">햇살이 뜨거울 땐 가려주고,</div>
-        <div className="content">비가 오면 우산이 되어주는</div>
-        <div className="content">부부가 되겠습니다.</div>
+        <div className="content">서로가 서로의 온 우주가 되어준 두 사람이</div>
+        <div className="content">이제 평생을 함께할 약속을 맺으려 합니다</div>
+        <div className="content">귀한 걸음 하시어 저희의 새로운 시작을</div>
         <div className="break" />
-        <div className="content">기쁜날 함께 하시어</div>
-        <div className="content">저희의 앞날을 축복해 주세요.</div>
+        <div className="content">따뜻한 축복으로 밝혀주시면</div>
+        <div className="content">더없는 기쁨이 되겠습니다.</div>
 
         <div className="break" />
 
